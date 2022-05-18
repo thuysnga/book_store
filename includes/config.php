f8d56afc4fd3fc6d890a671775a33327a114e129
ThuysNga <19521881@gm.uit.edu.vn>
2022-05-18 19:33:25 +0700

    done dbconnection

--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
+define('DB_NAME', 'book_store');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
